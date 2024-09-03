b8c093ab127ddfece3c76254f9c464859a4e380a
nishabisht <nishabisht223@gmail.com>
2024-09-03 14:35:53 +0530

    intial project commit

---
+++ book-nest/src/test/java/com/v1/book_nest/BookNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.book_nest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
